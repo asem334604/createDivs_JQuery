9b5446f1de0ef8d19b4a00100f9476db390d0ac8
Alexander Semyzhonov <a.sem334604@gmail.com>
2023-01-25 03:51:34 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+$('document').ready(function () {
+    $('#btn_show').on('click', function () {
+        let res = {};
+        $('input').each(function () {
+            res[$(this).attr('placeholder')] = $(this).val();
+        });
+        $('#playground').html('');
+        for (let i = 0; i < res['count']; i++) {
+            $('#playground').append(
+                `<div style="width: ${res['width']}px; 
+                height: ${res['height']}px; 
+                background-color: ${res['color']}; 
+                border-radius: ${res['radius']}px">  
+                </div>`);
+        }
+    })
+})
+
+
+
+// (()=>{
+//     const widthInput = document.getElementById('input_width');
+//     const heightInput = document.getElementById('input_height');
+//     const radiusInput = document.getElementById('input_radius');
+//     const countInput = document.getElementById('input_count');
+//     const colorInput = document.getElementById('input_color');
+//
+//     const playground = document.getElementById('playground');
+//
+//     document.getElementById('btn_show').addEventListener('click',()=>{
+//         playground.replaceChildren();
+//         //playground.innerHTML = '';
+//         const widthVal = widthInput.value;
+//         const heightVal = heightInput.value;
+//         const radiusVal = radiusInput.value;
+//         const countVal = Number(countInput.value);
+//         const colorVal = colorInput.value;
+//
+//         for (let i = 0; i < countVal; i++) {
+//             const newElement = document.createElement('div');
+//
+//             newElement.style.width = widthVal+"px";
+//             newElement.style.height = heightVal+"px";
+//             newElement.style.borderRadius = radiusVal+"px";
+//             newElement.style.backgroundColor = colorVal;
+//             //console.log(newElement);
+//             playground.appendChild(newElement);
+//         }
+//     })
+// })();
