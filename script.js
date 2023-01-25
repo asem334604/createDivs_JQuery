bd2724934e6f16b7ade77bad4c0094ea5fd2e83e
asem334604 <119743469+asem334604@users.noreply.github.com>
2023-01-25 03:54:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -50,33 +50,3 @@ $('document').ready(function () {
 
 
 
-// (()=>{
-//     const widthInput = document.getElementById('input_width');
-//     const heightInput = document.getElementById('input_height');
-//     const radiusInput = document.getElementById('input_radius');
-//     const countInput = document.getElementById('input_count');
-//     const colorInput = document.getElementById('input_color');
-//
-//     const playground = document.getElementById('playground');
-//
-//     document.getElementById('btn_show').addEventListener('click',()=>{
-//         playground.replaceChildren();
-//         //playground.innerHTML = '';
-//         const widthVal = widthInput.value;
-//         const heightVal = heightInput.value;
-//         const radiusVal = radiusInput.value;
-//         const countVal = Number(countInput.value);
-//         const colorVal = colorInput.value;
-//
-//         for (let i = 0; i < countVal; i++) {
-//             const newElement = document.createElement('div');
-//
-//             newElement.style.width = widthVal+"px";
-//             newElement.style.height = heightVal+"px";
-//             newElement.style.borderRadius = radiusVal+"px";
-//             newElement.style.backgroundColor = colorVal;
-//             //console.log(newElement);
-//             playground.appendChild(newElement);
-//         }
-//     })
-// })();
